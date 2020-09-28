a192b53fe1175b0e0da9f2dcd203916bd2d11760
varaprasad7007 <varaprasad7007@gmail.com>
2020-09-28 23:02:58 +0530

    style added

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
+import './Person.css';
 const person = (props) => {
-    return <div>
+    return <div className="Person">
         <p onClick={props.click}>Im {props.name} age {props.age}</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed}/>

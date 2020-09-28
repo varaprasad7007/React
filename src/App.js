76e22621e70b6778bb47912c55cf73282d99256a
varaprasad7007 <varaprasad7007@gmail.com>
2020-09-28 23:50:13 +0530

    inline css

--- src/App.js
+++ src/App.js
@@ -31,11 +31,19 @@ nameChangedHandler= (event) => {
 }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',   
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
        <h1>hi, I am React </h1>
        <p> this app is working paragraph</p>
-       <button onClick={() => this.switchNameHandler('rajshekar')}>switch Name</button>
+       <button style={style} onClick={() => this.switchNameHandler('rajshekar')}>switch Name</button>
        <Person 
        name = {this.state.persons[0].name} 
        age = {this.state.persons[0].age}/>

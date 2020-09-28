000cf94211d69566464413d879be42c98546df98
varaprasad7007 <varaprasad7007@gmail.com>
2020-09-28 17:36:35 +0530

    states is used to change data dynamically using this keyword inside component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'prasad', age:23},
+      {name: 'kishore', age:24},
+      {name: 'sai', age:24}
+    ]
+  }
   render() {
     return (
       <div className="App">
        <h1>hi, I am React </h1>
        <p> this app is working paragraph</p>
-       <Person name ="prasad" age = "23"/>
-       <Person name = "kishore" age = "24">My Hobbies: Racing</Person>
-       <Person name = "sai" age = "24"/>
+       <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+       <Person name = {this.state.persons[1].name} age = {this.state.persons[0].age}>My Hobbies: Racing</Person>
+       <Person name = {this.state.persons[2].name} age = {this.state.persons[0].age}/>
 
       </div>
     );

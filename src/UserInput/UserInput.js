6629fe987f200ba07419ddf0a35ccd25b290ed78
varaprasad7007 <varaprasad7007@gmail.com>
2020-09-30 11:37:32 +0530

    assignment1 completed

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -3,9 +3,13 @@ import React from 'react';
 const userInput = (props) => {
     const inputStyle = {
         border: '2px solid red'
-    }
-    return <input type="text" style={inputStyle} onChange={props.changed} value={props.currentvalue}/>;
+    };
+
+    return <input 
+        type="text" 
+        style={inputStyle}
+        onChange={props.changed} 
+        value={props.currentName} />;
 };
 
 export default userInput;
-

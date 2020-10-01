31c34a6d99ed6a6f6f28dd892d9b17db8f8408e8
varaprasad7007 <varaprasad7007@gmail.com>
2020-10-01 18:01:56 +0530

    conditional rendering added more elegant way

--- src/App.js
+++ src/App.js
@@ -48,20 +48,13 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
-
-    
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          { this.state.showPersons ?
-          <div>   
-          <Person
-           name={this.state.persons[0].name}
-           age={this.state.persons[0].age} />
+    let persons = null;
+    if( this.state.showPersons ) {
+      persons = (
+        <div>
+           <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
           <Person
             name={this.state.persons[1].name}
             age={this.state.persons[1].age}
@@ -70,10 +63,18 @@ class App extends Component {
           <Person
             name={this.state.persons[2].name}
             age={this.state.persons[2].age} />
-         </div> : null
-         }
-
-         {/* {persons} */}
+        </div>
+      );
+    }
+    
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
